2657509aec53afc723a4c2fa8362dcc483b65a31
Jake Thwaites <jakethwaites@hotmail.co.uk>
2018-10-19 20:21:49 +0100

    reformatted remove_pet_by_name method

--- pet_shop.rb
+++ pet_shop.rb
@@ -50,7 +50,7 @@ end
 def remove_pet_by_name(pet_shop, pet_name)
   for pet in pet_shop[:pets]
     if pet[:name] == pet_name
-      pet_shop[:pets].delete_at(pet_shop[:pets].index(pet))
+      pet_shop[:pets].delete(pet)
     end
   end
 end
